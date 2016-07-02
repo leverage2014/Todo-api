1bcd9890334dc9aee90b58af6128b75c73e1bff0
leverage2014 <myletter428@gmail.com>
2016-07-02 15:35:34 -0400

    add auth middleware and findByToken class method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var bcrypt = require('bcrypt');
 var db = require('./db');
+var middleware = require('./middleware')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -31,7 +32,7 @@ app.get('/', function(req, res){
 });
 
 // GET /todos?completed=true&q=work
-app.get('/todos', function(req, res){
+app.get('/todos', middleware.requireAuthentication, function(req, res){
 
 	var query = req.query;
 	var where = {};
@@ -59,7 +60,7 @@ app.get('/todos', function(req, res){
 });
 
 // GET /todos/:id
-app.get('/todos/:id', function(req, res){
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res){
 	var todoId = parseInt(req.params.id,10);
 
 	db.todo.findById(todoId).then(function(todo){
@@ -74,7 +75,7 @@ app.get('/todos/:id', function(req, res){
 });
 
 // POST /todos
-app.post('/todos', function(req, res){
+app.post('/todos', middleware.requireAuthentication, function(req, res){
 	//var body = req.body; // use _.pick to only pick description and completed
 	var body = _.pick(req.body, 'description', 'completed');
 
@@ -86,7 +87,7 @@ app.post('/todos', function(req, res){
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function(req, res){
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res){
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.destroy({
@@ -108,7 +109,7 @@ app.delete('/todos/:id', function(req, res){
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function(req, res){
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res){
 
 	var todoId = parseInt(req.params.id, 10);
 	var body = _.pick(req.body, 'description', 'completed');
